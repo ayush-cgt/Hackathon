b9d2eba30b7a573d48e1be370dc218796fe2d9b7
Sushil Prajapat <sushil.prajapat@cgt.co.in>
2016-09-10 18:02:28 +0530

    income

---
+++ app/src/main/java/com/android/myapplication/data/Type.java
@@ -0,0 +1,26 @@
+package com.android.myapplication.data;
+
+/**
+ * Created by sushil on 10/9/16.
+ */
+public class Type {
+
+    String id;
+    String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- app/src/main/java/com/android/myapplication/db/DBHelper.java
+++ app/src/main/java/com/android/myapplication/db/DBHelper.java
@@ -2,6 +2,10 @@ package com.android.myapplication.db;
 
 import android.content.Context;
 
+import com.android.myapplication.data.Type;
+
+import java.util.ArrayList;
+
 /**
  * Created by Ayush
  */
@@ -20,6 +24,27 @@ public class DBHelper {
         }
 
         return instance;
+    }
+
+
+    public ArrayList<Type> getTypes() {
+        ArrayList<Type> type = new ArrayList<Type>();
+
+        android.database.Cursor cursor = context.getContentResolver().query(DataProviderContract.IncomeMaster.CONTENT_URI, DataProviderContract.IncomeMaster.PROJECTION,
+                null, null, null);
+        if (cursor.getCount() > 0) {
+            int indexPID = cursor.getColumnIndex(DataProviderContract.IncomeMaster._ID);
+            int indexType = cursor.getColumnIndex(DataProviderContract.IncomeMaster.TYPE);
+
+            while (cursor.moveToNext()) {
+                Type t = new Type();
+                t.setId(cursor.getString(indexPID));
+                t.setType(cursor.getString(indexType));
+                type.add(t);
+            }
+        }
+        cursor.close();
+        return type;
     }
 
 
